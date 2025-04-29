3e435a3a34dc64fa1542ff25925bde0e33a99562
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2025-04-29 01:03:37 -0700

    remove delta and age from menu bar
    TODO: show age only if age > a constant

--- internal/nightscout/properties.go
+++ internal/nightscout/properties.go
@@ -15,10 +15,11 @@ func (p Properties) String(conf *config.Config) string {
 	result := p.Bgnow.DisplayBg(conf.Units) +
 		" " + p.Bgnow.Arrow(conf.Arrows)
 	if delta := p.Delta.Display(conf.Units); delta != "" {
-		result += " " + p.Delta.Display(conf.Units)
+		// result += " " + p.Delta.Display(conf.Units) // temporarily remove delta from title
 	}
 	if rel := p.Bgnow.Mills.Relative(conf.Advanced.RoundAge); rel != "" {
-		result += " [" + p.Bgnow.Mills.Relative(conf.Advanced.RoundAge) + "]"
+		// result += " [" + p.Bgnow.Mills.Relative(conf.Advanced.RoundAge) + "]" // temporarily remove age from title
+		// TODO: show age only if age > a constant
 	}
 	return result
 }
